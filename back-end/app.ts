ed720b4aa74410a313ba8c34b0891edce3482c82
Bruno <brunodinosantos@outlook.com>
2023-11-02 00:46:00 -0300

    fix: CORS policy

--- back-end/app.ts
+++ back-end/app.ts
@@ -8,18 +8,28 @@ import errorHandlingMiddleware from "./src/middlewares/errorHandling.middleware"
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:4000', 'https://shaw-and-partners-app.vercel.app/'];
+const allowedOrigins = [
+  "http://localhost:4000",
+  "https://shaw-and-partners-app.vercel.app/",
+];
 
-app.use(cors({
-    origin: function(origin, callback){
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
         return callback(new Error(msg), false);
       }
       return callback(null, true);
-    }
-  }));app.use(express.json());
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.use(requestLoggerMiddleware);
 
